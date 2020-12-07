474c22e58ef70c79870b8b52751b1d558e17daf7
Nagy <woundless17@gmail.com>
2020-12-07 13:39:21 +0100

    3dik nap

---
+++ 3_2020/solution.py
@@ -0,0 +1,40 @@
+file = open("input.txt","r")
+table = list()
+
+for line in file:
+    row = list()
+    for ch in line:
+        if ch != '\n':
+            row.append(ch)
+    
+    table.append(row)
+
+file.close()
+
+
+steps = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+tableSize = len(table)
+allTrees = 1
+
+for stp in steps:
+    print(stp)
+    treeCounter = 0
+    i = 0
+    j = 0
+    while i != tableSize-1:
+        
+        if (len(table[i])-1) < (j+stp[0]):
+            for k in range(i, tableSize):
+                table[k] += table[k]
+
+        j += stp[0]
+        i += stp[1]
+        print(str(i) + " " + str(j))
+
+        if table[i][j] == '#':
+            treeCounter += 1
+
+    allTrees *= treeCounter
+
+print("Vége: " + str(allTrees))
+
